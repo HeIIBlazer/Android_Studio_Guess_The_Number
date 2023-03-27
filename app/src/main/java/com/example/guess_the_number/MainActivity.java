a2bba770c7515b7e2219c5e123b9d772cecbd422
Exorcist <90632640+HeIIBlazer@users.noreply.github.com>
2023-03-27 14:36:25 +0300

    Done

---
+++ app/src/main/java/com/example/guess_the_number/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.guess_the_number;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int guess;
+    boolean gameFinished;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvInfo = (TextView)findViewById(R.id.textView);
+        etInput = (EditText)findViewById(R.id.editText);
+        bControl = (Button)findViewById(R.id.button);
+
+        guess = (int)(Math.random()*10);
+        gameFinished = false;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    public void sisend(View v){
+        if (!gameFinished){
+            int inp=Integer.parseInt(etInput.getText().toString());
+            if (inp > guess)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp < guess)
+                tvInfo.setText(getResources().getString(R.string.behind));
+            if (inp == guess)
+            {
+                tvInfo.setText(getResources().getString(R.string.hit));
+                bControl.setText(getResources().getString(R.string.play_more));
+                gameFinished = true;
+            }
+        }
+        else
+        {
+            guess = (int)(Math.random()*10);
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinished = false;
+        }
+        etInput.setText("");
+    }
+}
